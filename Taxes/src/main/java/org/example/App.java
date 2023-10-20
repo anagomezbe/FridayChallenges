b2ab6ac565850333166bc51e1deb0153912fc320
inda-fernandez <alvaritoacho@gmail.com>
2023-10-20 11:49:53 +0200

    Added some tax calculations uwu

--- Taxes/src/main/java/org/example/App.java
+++ Taxes/src/main/java/org/example/App.java
@@ -10,7 +10,9 @@ public class App {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Please enter you salary");
         try{
-            System.out.println("The amount of tax you pay is: " + taxBracket(myObj.nextInt()));
+            int salary = myObj.nextInt();
+            System.out.println("The tax bracket you are in is: " + taxBracket(salary) + "with a salary of" + salary);
+            System.out.println("You pay: " + getTaxAmount(taxBracket(salary), salary));
         } catch (Exception e) {
             System.out.println("Please only give an integer");
             System.out.println(e.getStackTrace());
@@ -20,29 +22,29 @@ public class App {
 
     public static int taxBracket(int salary) {
         int taxPercentage = 0;
+        double taxPaid = 0;
         if (salary < 0){
             System.out.println("Please give a positive number");
         } else if (salary >= 0 && salary < 15000){
             taxPercentage = 0;
-            System.out.println("You are in the " + taxPercentage + "% tax bracket");
+            taxPaid = 0;
         } else if (salary > 14999 && salary < 20000){
             taxPercentage = 10;
-            System.out.println("You are in the " + taxPercentage + "% tax bracket");
+            taxPaid = (salary - 14999) * (taxPercentage/100);
         } else if (salary > 19999 && salary < 30000){
             taxPercentage = 15;
-            System.out.println("You are in the " + taxPercentage + "% tax bracket");
-        } else if (salary > 29999 && salary < 45000) {
+            taxPaid =  499.90 + (salary - 19999) * (taxPercentage/100);
+        } else if (salary> 29999 && salary < 45000) {
             taxPercentage = 20;
-            System.out.println("You are in the " + taxPercentage + "% tax bracket");
+            taxPaid =  1999.75 + (salary - 29999) * (taxPercentage/100);
         } else if (salary > 44999) {
             taxPercentage = 25;
-            System.out.println("You are in the " + taxPercentage + "% tax bracket");
+            taxPaid = 5749.5 + (salary - 44999) * (taxPercentage/100);
         }
         int taxAmount = getTaxAmount(taxPercentage, salary);
         return taxAmount;
     }
-    public static int getTaxAmount( int taxPercentage, int salary) {
-
-        return 5;
-    }
+//    public static int getTaxAmount(int taxPercentage, int salary) {
+//        return salary * taxPercentage;
+//    }
 }
