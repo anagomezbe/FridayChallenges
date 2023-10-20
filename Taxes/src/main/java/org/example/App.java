83be6b828fb486634985453646866ee10dd6f192
inda-fernandez <alvaritoacho@gmail.com>
2023-10-20 12:14:25 +0200

    Made some changes to make code look prettier and de-coupled some components

--- Taxes/src/main/java/org/example/App.java
+++ Taxes/src/main/java/org/example/App.java
@@ -11,8 +11,11 @@ public class App {
         System.out.println("Please enter you salary");
         try{
             int salary = myObj.nextInt();
-            System.out.println("The tax bracket you are in is: " + taxBracket(salary) + "with a salary of" + salary);
-            System.out.println("You pay: " + getTaxAmount(taxBracket(salary), salary));
+            double bracket = taxBracket(salary);
+            double taxedMoney = getTaxAmount(bracket, salary);
+            System.out.println("The tax rate you are in is: " + bracket * 100 + "%, with a salary of: £" + salary);
+            System.out.println("The amount of tax you will pay is: £" + taxedMoney);
+            System.out.println("The money you will have left over is : £" + (salary - taxedMoney));
         } catch (Exception e) {
             System.out.println("Please only give an integer");
             System.out.println(e.getStackTrace());
@@ -20,31 +23,46 @@ public class App {
         }
     }
 
-    public static int taxBracket(int salary) {
-        int taxPercentage = 0;
-        double taxPaid = 0;
+    public static double taxBracket(int salary) {
+        double taxPercentage = 0.0;
+
         if (salary < 0){
             System.out.println("Please give a positive number");
         } else if (salary >= 0 && salary < 15000){
             taxPercentage = 0;
-            taxPaid = 0;
         } else if (salary > 14999 && salary < 20000){
-            taxPercentage = 10;
-            taxPaid = (salary - 14999) * (taxPercentage/100);
+            taxPercentage = 0.1;
         } else if (salary > 19999 && salary < 30000){
-            taxPercentage = 15;
-            taxPaid =  499.90 + (salary - 19999) * (taxPercentage/100);
+            taxPercentage = 0.15;
         } else if (salary> 29999 && salary < 45000) {
-            taxPercentage = 20;
-            taxPaid =  1999.75 + (salary - 29999) * (taxPercentage/100);
+            taxPercentage = 0.20;
         } else if (salary > 44999) {
-            taxPercentage = 25;
-            taxPaid = 5749.5 + (salary - 44999) * (taxPercentage/100);
+            taxPercentage = 0.25;
         }
-        int taxAmount = getTaxAmount(taxPercentage, salary);
-        return taxAmount;
+        return taxPercentage;
     }
-//    public static int getTaxAmount(int taxPercentage, int salary) {
-//        return salary * taxPercentage;
-//    }
+    public static double getTaxAmount(double bracket, int salary) {
+        double taxPaid = 0;
+        double bracket1 = 0, bracket2 = 0.10, bracket3 = 0.15, bracket4 = 0.2, bracket5 = 0.25;
+        double maxtax1 = 0 * bracket1;
+        double maxTax2 = maxtax1 + (20000 - 14999) * bracket2;
+        double maxTax3 = maxTax2 + (30000 - 19999) * bracket3;
+        double maxTax4 = maxTax3 + (45000 - 29999) * bracket4;
+        double maxTax5 = maxTax4 + (salary - 45000) * bracket5;
+
+        if (salary < 0){
+            System.out.println("Please give a positive number");
+        } else if (salary >= 0 && salary < 15000){
+            taxPaid = maxtax1;
+        } else if (salary > 14999 && salary < 20000){
+            taxPaid = maxtax1 + (salary - 15000) * bracket2;
+        } else if (salary > 19999 && salary < 30000){
+            taxPaid =  maxTax2 + (salary - 20000) * bracket3;
+        } else if (salary> 29999 && salary < 45000) {
+            taxPaid =  maxTax3 + (salary - 30000) * bracket4;
+        } else if (salary > 44999) {
+            taxPaid = maxTax5;
+        }
+        return taxPaid;
+    }
 }
